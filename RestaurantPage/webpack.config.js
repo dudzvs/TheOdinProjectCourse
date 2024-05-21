b90b8555e90c890f4ae15bcaaaa8b527b4152a45
Eduardo <eduardo_vteixeira@outlook.com>
2024-05-21 16:10:50 -0300

    Project structure

---
+++ RestaurantPage/webpack.config.js
@@ -0,0 +1,18 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clear: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      inject: 'head',
+      scriptLoading: 'defer',
+    }),
+  ],
+};
