b460ac18b7716c88e49bf3978c88e179bd4498bf
Eduardo <eduardo_vteixeira@outlook.com>
2024-07-17 10:39:44 -0300

    Implement set and get methods in hashMap class

--- HashMap/hashTable.js
+++ HashMap/hashTable.js
@@ -5,9 +5,39 @@ export class hashMap {
     this.genHashKey = genHashKey;
     this.table = new Array(initialCapacity).fill(null).map(() => []);
     this.capacity = initialCapacity;
+    this.size = 0;
   }
 
   _hash(key) {
     return this.genHashKey(key) % this.table.length;
+  }
+
+  set(key, value) {
+    if (key != null && value != null) {
+      const index = this._hash(key);
+      const bucket = this.table[index];
+
+      for (let i = 0; i < bucket.length; i++) {
+        if (bucket[i][0] === key) {
+          bucket[i][1] = value;
+          return;
+        }
+      }
+
+      bucket.push([key, value]);
+      this.size++;
+    }
+  }
+
+  get(key) {
+    const index = this._hash(key);
+    const bucket = this.table[index];
+
+    for (let i = 0; i < bucket.length; i++) {
+      if (bucket[i][0] === key) {
+        return bucket[i][1];
+      }
+    }
+    return null;
   }
 }
